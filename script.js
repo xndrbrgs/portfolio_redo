6da80ce0c29f0607adabb7419d7ca77065414109
xndrbrgs <aborgescolon@gmail.com>
2022-01-21 20:55:15 -0500

    trying to fix pc rip

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ function bgChanger() {
 }
 
 window.addEventListener("scroll", bgChanger);
+
+// Scroll Reveal
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true
+})
+
+sr.reveal('.nav-item');
